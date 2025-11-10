ef40bb1182680262253955210570619436e8fd1d
윤해인 <woovwave8.o0@gmail.com>
2025-11-10 18:04:51 +0900

    feat: 2. /introduce/string?name=이름 출력하기

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -2,14 +2,12 @@ package com.example.bcsd;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    //  @ResponseBody
+
     @GetMapping("/hello")
     public String hello() {
-//        return "Hello World"; //Hello World 라는 문자열을 반환
         return "hello"; //templates/hello.html 을 반환
     }
 }
--- src/main/java/com/example/bcsd/IntroduceController.java
+++ src/main/java/com/example/bcsd/IntroduceController.java
@@ -4,17 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @Controller
 public class IntroduceController {
     @GetMapping("/introduce/html")
     public String introduce() {
         return "introduce";
     }
-    // @GetMapping은 /greeting에 대한 HTTP GET 요청이 greeting() 메서드에 매핑되도록 합니다.
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+
+    // @GetMapping은 /introduce/에 대한 HTTP GET 요청이 introduce() 메서드에 매핑되도록 합니다.
+    @GetMapping("/introduce/string") // 쿼리는 여기에 미포함
+    public String introduce2(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "hello " + name + "!";
     }
 }
