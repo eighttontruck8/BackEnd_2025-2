d89f8fbb88eeeef6d3c9563d8ff1ce9188096008
윤해인 <woovwave8.o0@gmail.com>
2025-11-24 08:22:57 +0900

    refactor: 5주차 MVC 리팩토링 완료

---
+++ demo/demo/src/main/java/com/example/demo/repository/HelloRepository.java
@@ -0,0 +1,16 @@
+// (controller->) service -> db에 필요한 자료 요청
+// only 자료구조만 저장해놓자.
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HelloRepository {
+    public String getName() {
+        return "윤해인";
+    }
+
+    public int getAge() {
+        return 24;
+    }
+}
---
+++ demo/demo/src/main/java/com/example/demo/service/HelloService.java
@@ -0,0 +1,32 @@
+// service는 모델. 데이터 처리, 가공 등 실질적인 >로직<을 수행함
+// 모든 머리쓰는 것들은 service에게 맡겨주세요...
+
+package com.example.demo.service;
+import com.example.demo.repository.HelloRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class HelloService {
+    private final HelloRepository helloRepository;
+
+    // 생성자 주입
+    public HelloService(HelloRepository helloRepository){
+        this.helloRepository = helloRepository;
+    }
+    // 1) /introduce에 html 반환
+    // 필요없음
+
+    // 2) /introduce/string?name=이름 -> "안녕하세요. 제 이름은 000 입니다."
+    // 필요없음
+
+    // 3) /introduce/json -> {"name": "...", "age": ...}
+    public Map<String, Object> getHelloJson(){
+        Map<String, Object> output = new HashMap<>();
+        output.put("name", helloRepository.getName());
+        output.put("age", helloRepository.getAge());
+        return output;
+    }
+}
