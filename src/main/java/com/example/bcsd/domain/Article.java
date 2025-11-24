6fcc41a8e17b0c976ceae78959ec177e46176c6f
윤해인 <woovwave8.o0@gmail.com>
2025-11-24 19:42:45 +0900

    feat: Article 도메인 작성

--- src/main/java/com/example/bcsd/domain/Article.java
+++ src/main/java/com/example/bcsd/domain/Article.java
@@ -1,4 +1,15 @@
 package com.example.bcsd.domain;
 
+import java.time.LocalDateTime;
+
 public class Article {
+    private long id;
+    private long authorId;
+    private long boardId;
+    private String title;
+    private String content;
+    private LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
+
+    public Article(){}
 }
