421f6cf86c1c7cf248bf5496c60f23f6ac2b0292
윤해인 <woovwave8.o0@gmail.com>
2025-11-24 10:57:58 +0900

    refactor: hello 예제를 MVC모델로 리팩토링

---
+++ src/main/java/com/example/bcsd/controller/HelloController.java
@@ -0,0 +1,45 @@
+// controller는 어떤 로직을 호출할건지 요청을 받는다.
+// 즉, controller -> service
+
+package com.example.bcsd.controller;
+import com.example.bcsd.service.HelloService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/introduce")
+public class HelloController {
+
+    private final HelloService helloService;
+    public HelloController(HelloService helloService){
+        this.helloService = helloService;
+    }
+
+    // 1) /introduce/html -> introduce.html 반환
+    @GetMapping("/html")
+    public String hello(Model model) {
+        return "introduce";
+    }
+
+    // 2) /introduce/string?name=이름 -> "안녕하세요. 제 이름은 000 입니다."
+    @GetMapping("/string")
+    public String introduceString(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    // 3) /json에 json형태로 반환
+    @GetMapping("/json")
+    @ResponseBody
+    // <1> Map으로 JSON 수제로 만들기
+    public Map<String, Object> introduceJson() {
+        return helloService.getHelloJson();
+    }
+
+}
