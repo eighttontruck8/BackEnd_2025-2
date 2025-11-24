bba018ea9ba2b295c7abad9cd3d131eb7b47331c
윤해인 <woovwave8.o0@gmail.com>
2025-11-24 11:08:02 +0900

    refactor: DTO 추가

--- src/main/java/com/example/bcsd/controller/HelloController.java
+++ src/main/java/com/example/bcsd/controller/HelloController.java
@@ -1,7 +1,8 @@
 // controller는 어떤 로직을 호출할건지 요청을 받는다.
 // 즉, controller -> service
-
 package com.example.bcsd.controller;
+
+import com.example.bcsd.dto.HelloDTO;
 import com.example.bcsd.service.HelloService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
 
 @Controller
 @RequestMapping("/introduce")
@@ -37,9 +37,15 @@ public class HelloController {
     // 3) /json에 json형태로 반환
     @GetMapping("/json")
     @ResponseBody
-    // <1> Map으로 JSON 수제로 만들기
-    public Map<String, Object> introduceJson() {
-        return helloService.getHelloJson();
+    // <1> Map으로 JSON 수제로 만들기 -- 권장되는 방법이 아님
+//    public Map<String, Object> introduceJson() {
+//        return helloService.getHelloJson();
+//    }
+    // <2> 객체를 통해 JSON 자동 반환하기★
+    public HelloDTO getJson(){
+        HelloDTO hello = new HelloDTO();
+        hello.setAge(24);
+        hello.setName("윤해인");
+        return hello;
     }
-
 }
---
+++ src/main/java/com/example/bcsd/dto/HelloDTO.java
@@ -0,0 +1,19 @@
+package com.example.bcsd.dto;
+
+public class HelloDTO {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
