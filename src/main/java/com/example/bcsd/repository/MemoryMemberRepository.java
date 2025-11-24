0e4a15fc8c81ea59af6298489d868cf4241e406e
윤해인 <woovwave8.o0@gmail.com>
2025-11-24 13:37:16 +0900

    test: MemoryMemberRepositoryTest 추가

--- src/main/java/com/example/bcsd/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/bcsd/repository/MemoryMemberRepository.java
@@ -33,4 +33,8 @@ public class MemoryMemberRepository implements MemberRepository {
     public List<Member> findAll() {
         return new ArrayList<>(store.values()); // values가 member들임!
     }
+
+    public void clearStore(){
+        store.clear(); // 초기화
+    }
 }
---
+++ src/test/java/com/example/bcsd/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,4 @@
+package com.example.bcsd.repository;
+
+public class MemoryMemberRepositoryTest {
+}
