f576b72d4be6c24ab346873aaf47c550fc0d94b1
윤해인 <woovwave8.o0@gmail.com>
2026-01-05 00:05:56 +0900

    remove: articleRepository구현부 삭제

--- src/main/java/com/example/bcsd/repository/ArticleRepository.java
+++ src/main/java/com/example/bcsd/repository/ArticleRepository.java
@@ -1,24 +1,10 @@
 package com.example.bcsd.repository;
 
 import com.example.bcsd.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ArticleRepository {
-    // 1. GET(조회)
-    // b. boardId로 게시판의 게시물들을 JSON 배열로 반환
-    // /articles?boardId={boardId}
-    List<Article> findByBoardId(Long boardId);
+public interface ArticleRepository extends JpaRepository<Article,Integer> {
 
-    // c. article의 id로 해당 article 하나 조회
-    Article findById(Long id); //ID로 회원 정보 가져오기
-
-    // 2. POST(등록)
-    Article insert(Article article);
-
-    // 3. PUT(수정)
-    Article update(Article article);
-
-//    // 4. DELETE(삭제)
-//    void deleteById(Long id);
 }
