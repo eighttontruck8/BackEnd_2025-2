4fc9db80b1f64252b9275462fecfa5c54abaf6ef
윤해인 <woovwave8.o0@gmail.com>
2026-01-04 23:06:57 +0900

    remove: boardRepository 구현부 삭제

--- src/main/java/com/example/bcsd/repository/BoardRepository.java
+++ src/main/java/com/example/bcsd/repository/BoardRepository.java
@@ -4,13 +4,5 @@ import com.example.bcsd.dto.BoardDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
-//    Board findById(Long id);
-
-    // 1. 생성
-    Board insert(BoardDTO board);
-
-    // 2. 삭제
-    boolean delete(Long id);
-
-    boolean existsById(Long id);
+    // 내용 전부 삭제. JPA가 대체.
 }
