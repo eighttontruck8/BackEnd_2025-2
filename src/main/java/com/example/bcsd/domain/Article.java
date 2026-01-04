584c45ca68f0120793db50095bdd72e327b39a3b
윤해인 <woovwave8.o0@gmail.com>
2026-01-04 22:47:42 +0900

    refactor: article테이블에 jpa 추가

--- src/main/java/com/example/bcsd/domain/Article.java
+++ src/main/java/com/example/bcsd/domain/Article.java
@@ -1,11 +1,20 @@
 package com.example.bcsd.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-
+@Setter
+@Getter
+@Entity
+@Table(name = "article")
 public class Article {
+    @Id
     private Long id;
     @JsonProperty("author_id")
     private Long authorId;
@@ -19,61 +28,4 @@ public class Article {
     private LocalDateTime createdAt;
 
     public Article(){}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(Long authorId) {
-        this.authorId = authorId;
-    }
-
-    public Long getBoardId() {
-        return boardId;
-    }
-
-    public void setBoardId(Long boardId) {
-        this.boardId = boardId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
 }
