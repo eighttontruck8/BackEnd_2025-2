709f5b78cd1774b9adef2035ba69886362af1a90
윤해인 <woovwave8.o0@gmail.com>
2026-01-14 16:04:11 +0900

    feat: 10주차까지의 과제

--- src/main/java/com/example/bcsd/repository/ArticleRepository.java
+++ src/main/java/com/example/bcsd/repository/ArticleRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article,Long> {
-    boolean existsByBoardId(Long id);
     boolean existsByAuthorId(Long authorId);
     List<Article> findByBoardId(Long boardId);
 }
